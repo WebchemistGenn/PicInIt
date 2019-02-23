16667efce2c37d8ebf1116fe7d78ad698b594966
yoonwonyoul <yoonwonyoul@webchemist.net>
2019-02-23 23:07:50 +0900

    feat: front tslint config

--- FrontEnd/src/Root.tsx
+++ FrontEnd/src/Root.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const InitStyle = createGlobalStyle`
+  ${reset}
+`;
 
 const Root = () => {
-  return <div>aaaa</div>;
+  return (
+    <React.Fragment>
+      <InitStyle />
+      <div>aaaa</div>
+    </React.Fragment>
+  );
 };
 
 export default Root;
